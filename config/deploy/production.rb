45672a0c77349be7c40a9daa218694c59286b2a1
Bruno Santos <brunoasantos2@gmail.com>
2014-11-20 15:12:34 -0300

    Change deploy path

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,6 +4,8 @@ role :db,  %w{bitcoin@104.236.4.31}
 
 server '104.236.4.31', user: 'bitcoin', roles: %w{web app}
 
+set :deploy_to, "/home/bitcoin_futures"
+
 set :ssh_options, {
   forward_agent: true,
   port: 22,
