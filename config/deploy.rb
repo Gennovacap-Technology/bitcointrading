01161f2e6cc15e78156577e9a55a157e94706442
Bruno Santos <brunoasantos2@gmail.com>
2014-11-20 14:27:43 -0300

    Add deploy with capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+set :application, 'bitcoin-futures'
+set :repo_url, 'git@bitbucket.org:brunoasantos/bitcoin-futures.git'
+
+# namespace :deploy do
+
+#   desc 'Restart application'
+#   task :restart do
+#     on roles(:app), in: :sequence, wait: 5 do
+#       # Your restart mechanism here, for example:
+#       # execute :touch, release_path.join('tmp/restart.txt')
+#     end
+#   end
+
+#   after :publishing, :restart
+
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#       # Here we can do anything such as:
+#       # within release_path do
+#       #   execute :rake, 'cache:clear'
+#       # end
+#     end
+#   end
+
+# end
---
+++ config/deploy/production.rb
@@ -0,0 +1,10 @@
+role :app, %w{bitcoin@104.236.4.31}
+role :web, %w{bitcoin@104.236.4.31}
+role :db,  %w{bitcoin@104.236.4.31}
+
+server '104.236.4.31', user: 'bitcoin', roles: %w{web app}
+
+set :ssh_options, {
+  forward_agent: true,
+  port: 22
+}
