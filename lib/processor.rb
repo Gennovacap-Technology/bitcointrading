b0a48338753e5e720ee21b25256fc4adab1d5129
Bruno Santos <brunoasantos2@gmail.com>
2014-12-23 08:05:52 -0300

    Add ok_btcusd_ticker to list of channels

--- lib/processor.rb
+++ lib/processor.rb
@@ -10,7 +10,8 @@ class Processor < Server
     Server.new('processor').run! do
       EM.run do
         pg = PG.connect(host: 'localhost', dbname: 'bitcoin_futures', user: 'postgres', password: 'postgres')
-        pg.prepare('insert_data', 'INSERT INTO "data" ("channel", "buy", "contract_id", "high", "hold_amount", "last", "low", "sell", "unit_amount", "vol") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);')
+        pg.prepare('insert_data_future', 'INSERT INTO "data" ("channel", "buy", "contract_id", "high", "hold_amount", "last", "low", "sell", "unit_amount", "vol") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);')
+        pg.prepare('insert_data_current', 'INSERT INTO "current_data" ("channel", "buy", "high", "last", "low", "sell", "timestamp", "vol") VALUES ($1, $2, $3, $4, $5, $6, $7, $8);')
 
         redis = Redis.new
         redis.subscribe(*CHANNELS) do |on|
@@ -22,19 +23,31 @@ class Processor < Server
             Log::write "##{channel}: #{message}"
 
             data = JSON.parse(message)
-            pg.exec_prepared('insert_data', [
-              channel,
-              data['buy'],
-              data['contractId'],
-              data['high'],
-              data['hold_amount'],
-              data['last'],
-              data['low'],
-              data['sell'],
-              data['unitAmount'],
-              data['vol']
-            ])
-
+            if channel == 'ok_btcusd_ticker' # current
+              pg.exec_prepared('insert_data_current', [
+                channel,
+                data['buy'],
+                data['high'],
+                data['last'],
+                data['low'],
+                data['sell'],
+                data['timestamp'].to_i,
+                data['vol']
+              ])
+            else # future
+              pg.exec_prepared('insert_data_future', [
+                channel,
+                data['buy'],
+                data['contractId'],
+                data['high'],
+                data['hold_amount'],
+                data['last'],
+                data['low'],
+                data['sell'],
+                data['unitAmount'],
+                data['vol']
+              ])
+            end
             redis.unsubscribe if message == "exit"
           end
 
--- lib/server.rb
+++ lib/server.rb
@@ -11,7 +11,8 @@ CHANNELS = [
   'ok_btcusd_future_ticker_this_week',
   'ok_btcusd_future_ticker_next_week',
   'ok_btcusd_future_ticker_month',
-  'ok_btcusd_future_ticker_quarter'
+  'ok_btcusd_future_ticker_quarter',
+  'ok_btcusd_ticker'
 ]
 
 class Server
